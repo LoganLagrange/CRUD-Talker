6ce97cf4ff7bd2e0b9e1de8302d5d1a99afb908a
Logan Lagrange <lagrangelogan@gmail.com>
2023-11-24 17:58:58 -0500

    added index.js with all models, added to server file, tables successfully build on server startup

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 const User = require(`./User`);
+const Message = require(`./Message`);
+const Conversation = require(`./Conversation`);
 
 module.exports = {
-    User
+    User,
+    Message,
+    Conversation
 }
--- server.js
+++ server.js
@@ -12,7 +12,7 @@
  const app = express();
  const PORT = process.env.PORT || 3000;
  // Requiring our models for syncing
- const {User} = require('./models');
+ const {User, Message, Conversation} = require('./models');
 //  const sess = {
 //     secret: 'Super secret secret',
 //     cookie: {
