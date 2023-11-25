3b582b830d404045f9c2d1a417cf44c504f7b4a7
Logan Lagrange <lagrangelogan@gmail.com>
2023-11-25 14:55:40 -0500

    added message routes and enabled sessions

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const userRoutes = require(`./userRoutes`)
 router.use(`/api/users`,userRoutes);
 
+const messageRoutes = require(`./messageRoutes`)
+router.use(`/api/messages`,messageRoutes);
 
 
 module.exports = router;
---
+++ controllers/messageRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const { Message, User } = require('../models'); // Imports the Message model
+
+// GET all messages
+router.get(`/`,(req,res) => {
+    Message.findAll().then(dbMessage => {
+        res.json(dbMessage);
+    }).catch(err => {
+        res.status(500).json({msg:`Server Error!`, err});
+    })
+})
+
+// GET one message
+router.get(`/:id`,(req,res) => {
+    Message.findByPk(req.params.id,{
+        include:[User]
+    }).then(dbMessage => {
+       if(!dbMessage) {
+        return res.status(404).json(`No message exists!`)
+       }
+       res.json(dbMessage)
+    }).catch(err => {
+        res.status(500).json({msg:`Server Error!`, err});
+        console.log(err);
+    })
+})
+
+// CREATE new message
+router.post(`/`,(req,res) => {
+    Message.create({
+        content:req.body.content,
+        user_id:req.body.user_id,
+        conversation_id:req.body.conversation_id
+    }).then(dbMessages => {
+        res.json(dbMessages)
+    }).catch(err => {
+        res.status(500).json({msg:`Server error!`,err});
+    })
+});
+
+module.exports = router;
