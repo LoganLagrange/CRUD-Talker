b8a2920492541591ed0d4473958ed0cf8e03ac6e
Rengo _ <renatovaldez6879@gmail.com>
2023-11-27 21:26:01 -0500

    added handlebar routes

---
+++ controllers/htmlRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { Conversation, Message, User, Friend } = require("../models");
+
+router.get("/login",(req,res)=>{
+    res.render("login_page")
+})
+
+router.get('/conversations',(req,res)=>{
+    res.render("chat_room_page")
+})
+
+router.get('/registration',(req,res)=>{
+    res.render("registration_page")
+})
