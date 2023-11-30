724a6b1fbc1d372b1a59b0af450e311334208d0b
Logan Lagrange <lagrangelogan@gmail.com>
2023-11-29 22:43:39 -0500

    added renderotherchats function to render other chats in sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -7,21 +7,48 @@ fetch(url, {
     headers: {
         "Content-Type": "application/json",
     },
-    
-}).then(res => res.json())
-.then(res => {
-    console.log(res)
-    renderYourChats(res);
-}) .catch(err => {
-    console.error(err);
-});
 
-function renderYourChats(chats){
-    if(chats){
+}).then(res => res.json())
+    .then(res => {
+        console.log(res)
+        renderYourChats(res);
+    }).catch(err => {
+        console.error(err);
+    });
+
+function renderYourChats(chats) {
+    if (chats) {
         chats.forEach(item => {
             const yourChatsLi = document.createElement(`li`);
             yourChatsLi.textContent = `${item.conversation_name}`
             yourChatsOl.appendChild(yourChatsLi);
+        });
+    }
+}
+
+const otherUrl = `http://localhost:3000/api/conversations/isin`
+fetch(otherUrl, {
+    method: `GET`,
+    headers: {
+        "Content-Type": "application/json",
+    },
+
+}).then(res => res.json())
+    .then(res => {
+        console.log(res)
+        renderOtherChats(res);
+    }).catch(err => {
+        console.error(err);
+    });
+
+function renderOtherChats(chats) {
+    if (chats) {
+        chats.forEach(item => {
+            if (item.ownerId !== item.userId) {
+                const otherChatsLi = document.createElement(`li`);
+                otherChatsLi.textContent = `${item.conversation_name}`
+                otherChatsOl.appendChild(otherChatsLi);
+            }
         });
     }
 }
