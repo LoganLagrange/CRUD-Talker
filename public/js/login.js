c2ffe70b1263a0b4735fb5262bdff0708223ccd4
Logan Lagrange <lagrangelogan@gmail.com>
2023-12-01 20:18:54 -0500

    cleaned up and added comments

--- public/js/login.js
+++ public/js/login.js
@@ -4,11 +4,15 @@ const loginUsername = document.getElementById(`login-username`);
 const loginPassword  = document.getElementById(`login-password`);
 const loginWarning =  document.getElementById(`login-warning`);
 
+// Event listener for login form
 loginForm.addEventListener(`submit`, (e) =>  {
     e.preventDefault();
+    // calls login function
     loginFunc(loginUsername.value, loginPassword.value);
 })
 
+// Login function
+// issues post request to server to log user in (create session variables)
 function loginFunc(username, password){
     const url = `http://localhost:3000/api/users/login`
     
@@ -23,6 +27,8 @@ function loginFunc(username, password){
         },
         body: JSON.stringify(user),
     }).then(res => {
+        // checks if server sent bad status code
+        // rejects if it did to trigger catch block for error
         if(!res.ok) {
             if(res.status === 401) {
                 return Promise.reject({status: 401, message: `Incorrect username or password!`})
@@ -35,9 +41,8 @@ function loginFunc(username, password){
         const newUrl = `http://localhost:3000/`
         window.location.href = newUrl;
     }) .catch(err => {
+        // logs and displays error or bad status code
         console.error(err);
         loginWarning.textContent =  err.message;
     });
-
-    
 }
