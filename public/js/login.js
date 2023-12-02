2b25d934aab01d929f5c67a4571465be38c598b9
Logan Lagrange <lagrangelogan@gmail.com>
2023-12-01 20:16:01 -0500

    improved login error handling, displays incorrect uername or password to user

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,8 @@
-
-
 // LOGIN PAGE logic
 const loginForm = document.getElementById(`login-form`);
 const loginUsername = document.getElementById(`login-username`);
 const loginPassword  = document.getElementById(`login-password`);
+const loginWarning =  document.getElementById(`login-warning`);
 
 loginForm.addEventListener(`submit`, (e) =>  {
     e.preventDefault();
@@ -23,12 +22,21 @@ function loginFunc(username, password){
             "Content-Type": "application/json",
         },
         body: JSON.stringify(user),
-    }).then(res => res.json())
-    .then(res => {
+    }).then(res => {
+        if(!res.ok) {
+            if(res.status === 401) {
+                return Promise.reject({status: 401, message: `Incorrect username or password!`})
+            } else {
+                return Promise.reject({status: res.status, message: `Unexpected error occurred!`})
+            }
+        }
+        return res.json;
+    }).then(res => {
         const newUrl = `http://localhost:3000/`
         window.location.href = newUrl;
     }) .catch(err => {
         console.error(err);
+        loginWarning.textContent =  err.message;
     });
 
     
